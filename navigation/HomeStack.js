b7898d935919ec950208ce2d73112b759934f6e7
James Yeates <jimibue@gmail.com>
2020-04-15 12:24:15 -0600

    done

--- navigation/HomeStack.js
+++ navigation/HomeStack.js
@@ -1,15 +1,15 @@
-import * as React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from '../screens/HomeScreen';
-import EmployeeDetail from '../screens/EmployeeDetail';
+import * as React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "../screens/HomeScreen";
+import EmployeeDetail from "../screens/EmployeeDetail";
 
 const Stack = createStackNavigator();
 
 export default function HomeStack() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator  headerMode="none">
       <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Employee" component={EmployeeDetail} /> 
+      <Stack.Screen name="Employee" component={EmployeeDetail} />
     </Stack.Navigator>
   );
 }
--- screens/EmployeeDetail.js
+++ screens/EmployeeDetail.js
@@ -1,9 +1,15 @@
 import React from 'react'
-import { View,Text } from 'react-native'
-
+import { View,Text, Button } from 'react-native'
+import { useNavigation } from '@react-navigation/native';
 export default function EmployeeDetail(){
+  
+  const navigation = useNavigation()
   return(
     <View>
+      <Button 
+        onPress={()=> navigation.goBack()}
+        title="Go back"
+      />
       <Text>yo EmployeeDetail</Text>
     </View>
   )
